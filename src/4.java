8ee87b64332f3d539136b7e64734b241416bbfb5
jroenbojic <jroenbojic@gmail.com>
2025-03-09 19:22:33 +0000

    Add 4.java

---
+++ src/4.java
@@ -0,0 +1,11 @@
+import java.util.Random;
+
+class RandomMath {
+
+    public static void main(String[] args) {
+        Random rand = new Random();
+        int number1 = rand.nextInt(10);
+        int number2 = rand.nextInt(10);
+        System.out.println("What is " + number1 + " + " + number2 + "?");
+    }
+}
