13bff1a75f1a58d19a62f04c06fa06205288d3cd
jroenbojic <jroenbojic@gmail.com>
2025-03-12 06:00:36 +0000

    Add 7.java

---
+++ src/7.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.lang.*;
+public class MathHomework {
+    public static void main (String args[]) {
+        int a = 20;
+        int b = 10;
+        double c = (double) a / b;
+        System.out.println(c);
+    }
+}
