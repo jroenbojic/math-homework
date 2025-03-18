61513437ed54e81f330ea0326b4e5bc3a9445301
jroenbojic <jroenbojic@gmail.com>
2025-03-18 01:01:53 +0000

    Add 13.java

---
+++ src/13.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class MathHomework {
+  public static void main(String[] args) {
+    int x = 5;
+    int y = 7;
+    int result = add(x, y);
+    System.out.println("The sum of " + x + " and " + y + " is: " + result);
+  }
+
+  public static int add(int a, int b) {
+    return a + b;
+  }
+}
