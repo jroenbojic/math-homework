84cb70e38e880ab614199a456aadf547dc01a098
jroenbojic <jroenbojic@gmail.com>
2025-03-29 18:52:45 +0000

    Add 20.java

---
+++ src/20.java
@@ -0,0 +1,7 @@
+public class WeeklyMathHomework {
+    public static void main(String[] args) {
+        // Code to solve weekly math problems here
+        System.out.println("This is your weekly math homework.");
+        // Add more codes for solving math problems here
+    }
+}
