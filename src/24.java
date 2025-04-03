8bfafa63fa844cceb4fa3ac221177df99b2d296a
jroenbojic <jroenbojic@gmail.com>
2025-04-03 16:35:52 +0000

    Add 24.java

---
+++ src/24.java
@@ -0,0 +1,21 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Example calculation 1: Addition
+        int num1 = 5;
+        int num2 = 3;
+        int result = num1 + num2;
+        System.out.println("The sum of " + num1 + " and " + num2 + " is " + result);
+
+        // Example calculation 2: Subtraction
+        int num3 = 7;
+        int num4 = 2;
+        int result = num3 - num4;
+        System.out.println("The difference between " + num3 + " and " + num4 + " is " + result);
+
+        // Example calculation 3: Multiplication
+        int num5 = 8;
+        int num6 = 4;
+        int result = num5 * num6;
+        System.out.println("The product of " + num5 + " and " + num6 + " is " + result);
+    }
+}
