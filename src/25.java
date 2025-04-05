f607813279b14b40a3046cd209b17c267f1aba39
jroenbojic <jroenbojic@gmail.com>
2025-04-05 06:13:39 +0000

    Add 25.java

---
+++ src/25.java
@@ -0,0 +1,14 @@
+public class WeeklyMathHomework {
+    public static void main(String[] args) {
+        // Add your daily math problems here
+        solveDailyMathProblem();
+    }
+
+    private static void solveDailyMathProblem() {
+        int num1 = 5;
+        int num2 = 3;
+        int sum = num1 + num2;
+
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}
