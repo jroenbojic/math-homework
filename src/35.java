699fb2bda7525bef22a7b56af5f9333f5381a62c
jroenbojic <jroenbojic@gmail.com>
2025-05-17 07:15:23 +0000

    Add 35.java

---
+++ src/35.java
@@ -0,0 +1,8 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Code to solve math problems
+        System.out.println("Hello, world!");
+        double result = 5 + 3;
+        System.out.println("The sum of 5 and 3 is: " + result);
+    }
+}
