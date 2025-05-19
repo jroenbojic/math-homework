202aa5f01b96428d8c72bddb24255a064af6a9d1
jroenbojic <jroenbojic@gmail.com>
2025-05-19 21:28:34 +0000

    Add 38.java

---
+++ src/38.java
@@ -0,0 +1,15 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Example 1: Addition
+        System.out.println("Adding 5 and 7: " + (5 + 7));
+
+        // Example 2: Subtraction
+        System.out.println("Subtracting 3 from 8: " + (8 - 3));
+
+        // Example 3: Multiplication
+        System.out.println("Multiplying 6 by 4: " + (6 * 4));
+
+        // Example 4: Division
+        System.out.println("Dividing 9 by 3: " + (9 / 3));
+    }
+}
