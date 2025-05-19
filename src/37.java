e0e42f39c2550a6055543f216257a8396b20bf2a
jroenbojic <jroenbojic@gmail.com>
2025-05-19 19:19:49 +0000

    Add 37.java

---
+++ src/37.java
@@ -0,0 +1,18 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Example of simple mathematical operations
+        int result = add(3, 4);
+        System.out.println("The sum is: " + result);
+
+        double product = multiply(5, 6);
+        System.out.println("The product is: " + product);
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static double multiply(double x, double y) {
+        return x * y;
+    }
+}
