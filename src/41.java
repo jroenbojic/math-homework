06a312a3132a78359643c53b5d14d0fb7ca073ef
jroenbojic <jroenbojic@gmail.com>
2025-05-24 16:18:21 +0000

    Add 41.java

---
+++ src/41.java
@@ -0,0 +1,7 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Example mathematical problem solving code
+        double result = 3.14 * 5 - (2 + 3) * 4;
+        System.out.println("The result is: " + result);
+    }
+}
